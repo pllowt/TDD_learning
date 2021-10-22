06eed9e827f539c51da7150683fa23cd4c58bef5
lowth01 <paul.lowther01@hotmail.com>
2021-10-22 16:24:03 +0100

    initial Commit

---
+++ ner_client.py
@@ -0,0 +1,9 @@
+import spacy
+
+class NamedEntityClient:
+    def __init__(self, model):
+        self.model = model
+
+    def get_ents(self, sentence):
+        doc = self.model(sent)
+        return {'ents': [], 'html': ''}
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+        name="flaskner",
+        version="0.01",
+        description="A simple NER API by P.Lowther"
+)
+
